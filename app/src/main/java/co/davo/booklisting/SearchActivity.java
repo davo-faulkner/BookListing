f3e5542b3c6562b8ea295ed9a2d55ae2d434ae8b
Davo Faulkner <d@davo.co>
2017-05-18 17:14:17 -0500

    Add searchEditText and make keyboard visible

--- app/src/main/java/co/davo/booklisting/SearchActivity.java
+++ app/src/main/java/co/davo/booklisting/SearchActivity.java
@@ -1,10 +1,14 @@
 package co.davo.booklisting;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -17,6 +21,9 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        EditText searchEditText = (EditText) findViewById(R.id.search_editText);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
 
         Button searchButton = (Button) findViewById(R.id.search_button);
         searchButton.setOnClickListener(new View.OnClickListener() {
