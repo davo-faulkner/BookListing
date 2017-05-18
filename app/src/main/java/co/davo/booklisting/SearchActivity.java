71e1550755e5581f7894cd5f3fed19a5271c13eb
Davo Faulkner <d@davo.co>
2017-05-18 00:39:30 -0500

    Add searchButton OnClickListener to SearchActivity.java

--- app/src/main/java/co/davo/booklisting/SearchActivity.java
+++ app/src/main/java/co/davo/booklisting/SearchActivity.java
@@ -1,7 +1,9 @@
 package co.davo.booklisting;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        Button searchButton = (Button) findViewById(R.id.search_button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
