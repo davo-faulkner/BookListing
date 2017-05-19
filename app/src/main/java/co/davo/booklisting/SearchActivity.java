bf32e29da40983f3de5116761d8cce2e198e2a63
Davo Faulkner <d@davo.co>
2017-05-19 00:33:34 -0500

    Build mQueryUrl String

--- app/src/main/java/co/davo/booklisting/SearchActivity.java
+++ app/src/main/java/co/davo/booklisting/SearchActivity.java
@@ -1,12 +1,10 @@
 package co.davo.booklisting;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -22,7 +20,7 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        EditText searchEditText = (EditText) findViewById(R.id.search_editText);
+        final EditText searchEditText = (EditText) findViewById(R.id.search_editText);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
 
         Button searchButton = (Button) findViewById(R.id.search_button);
@@ -30,6 +28,8 @@ public class SearchActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
+                mQueryUrl = "https://www.googleapis.com/books/v1/volumes?q=" + searchEditText.getText().toString() + "&maxResults=10";
+
                 Intent resultsIntent = new Intent(SearchActivity.this, ResultsActivity.class);
                 startActivity(resultsIntent);
 
