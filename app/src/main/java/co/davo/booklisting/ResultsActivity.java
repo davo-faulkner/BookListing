8a334d03d1224d22a9c881f0aba6e2b81f379696
Davo Faulkner <d@davo.co>
2017-05-22 17:02:30 -0500

    Add Network State Test, ProgressBar to ResultsActivity.java

--- app/src/main/java/co/davo/booklisting/ResultsActivity.java
+++ app/src/main/java/co/davo/booklisting/ResultsActivity.java
@@ -1,9 +1,14 @@
 package co.davo.booklisting;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -20,6 +25,30 @@ public class ResultsActivity extends AppCompatActivity implements LoaderManager.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        ListView bookListView = (ListView) findViewById(R.id.list);
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        bookListView.setEmptyView(mEmptyStateTextView);
+
+        ConnectivityManager cm =
+                (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+
+        mProgressBar = (ProgressBar) findViewById(R.id.loading_spinner);
+
+        mBookAdapter = new BookAdapter(this, new ArrayList<Book>());
+
+        LoaderManager loaderManager = getLoaderManager();
+
+        if (!isConnected) {
+            mProgressBar.setVisibility(View.GONE);
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        } else {
+            
+        }
     }
     @Override
     public Loader<ArrayList<Book>> onCreateLoader(int id, Bundle args) {
