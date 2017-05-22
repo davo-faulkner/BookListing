d1d903afecccc672f6b792a0b7f9bfcea4edf295
Davo Faulkner <d@davo.co>
2017-05-22 16:46:03 -0500

    Add Loader Methods

--- app/src/main/java/co/davo/booklisting/ResultsActivity.java
+++ app/src/main/java/co/davo/booklisting/ResultsActivity.java
@@ -1,13 +1,29 @@
 package co.davo.booklisting;
 
+import android.app.LoaderManager;
+import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ResultsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class ResultsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<Book>> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+    }
+    @Override
+    public Loader<ArrayList<Book>> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+    @Override
+    public void onLoadFinished(Loader<ArrayList<Book>> loader, ArrayList<Book> data) {
+
+    }
+    @Override
+    public void onLoaderReset(Loader<ArrayList<Book>> loader) {
+
     }
 }
