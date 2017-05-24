1743934385eb79fdd0432e2492b9043c2c5477a3
Davo Faulkner <d@davo.co>
2017-05-24 00:37:58 -0500

    Everything but the date and layout (runs)

--- app/src/main/java/co/davo/booklisting/BookAdapter.java
+++ app/src/main/java/co/davo/booklisting/BookAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -29,6 +30,30 @@ public class BookAdapter extends ArrayAdapter<Book> {
                     R.layout.list_item, parent, false);
         }
         Book currentBook = getItem(position);
+        
+        TextView titleTextView = (TextView) listItemView.findViewById(R.id.title);
+        titleTextView.setText(currentBook.getTitle());
+        
+        TextView authorTextView = (TextView) listItemView.findViewById(R.id.author);
+        String authorsString = "";
+        ArrayList<String> authors = currentBook.getAuthors();
+        for (int i = 0; i < authors.size(); i++) {
+            authorsString = authorsString + currentBook.getAuthors().get(i);
+            if (authors.size() - i != 1) {
+                authorsString = authorsString + "\n";
+            }
+        }
+        authorTextView.setText(authorsString);
+
+        TextView publishedDateTextView = (TextView) listItemView.findViewById(R.id.published_date);
+        String publishedDateString = "";
+        publishedDateTextView.setText("Jan 1, 1970");
+
+        TextView pageCountTextView = (TextView) listItemView.findViewById(R.id.page_count);
+        pageCountTextView.setText(currentBook.getPageCount() + " pages");
+
+        TextView descriptionTextView = (TextView) listItemView.findViewById(R.id.description);
+        descriptionTextView.setText(currentBook.getDescription());
 
         return listItemView;
     }
